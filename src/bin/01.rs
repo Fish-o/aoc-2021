f472e6f4fccab09f10d2a95250f076ffc71a344b
Conner Hof <c.l.hof@student.utwente.nl>
2024-11-26 14:14:49 +0100

    Added tests for day 1 and 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -3,27 +3,22 @@ use itertools::Itertools;
 use rayon::prelude::*;
 advent_of_code::solution!(1);
 
-
-struct Data {
-    a: u32,
-}
 pub fn part_one(input: &str) -> Option<i32> {
-    let r = parsing::to_int_vec(input)
-        .windows(2)
-        .fold(0i32, |r, p| if p[0] < p[1] { r + 1 } else { r });
-    
-    Some(r)
-
-
+    Some(
+        parsing::to_int_vec(input)
+            .windows(2)
+            .fold(0i32, |r, p| if p[0] < p[1] { r + 1 } else { r }),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let r = parsing::to_int_vec(input)
-        .windows(3)
-        .map(|w| w[0] + w[1] + w[2])
-        .tuple_windows()
-        .fold(0u32, |r, w: (i64, i64)| if w.0 < w.1 { r + 1 } else { r });
-    Some(r)
+    Some(
+        parsing::to_int_vec(input)
+            .windows(3)
+            .map(|w| w[0] + w[1] + w[2])
+            .tuple_windows()
+            .fold(0u32, |r, w: (i64, i64)| if w.0 < w.1 { r + 1 } else { r }),
+    )
 }
 
 #[cfg(test)]
@@ -33,12 +28,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(7));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(5));
     }
 }
--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,4 +1,3 @@
-
 use std::fmt::{Debug, Display};
 
 use advent_of_code::utils::parsing;
@@ -6,23 +5,23 @@ use itertools::Itertools;
 use rayon::prelude::*;
 advent_of_code::solution!(2);
 
-pub fn part_one(input: &str) -> Option<impl Debug + Display + num::Integer> { 
+pub fn part_one(input: &str) -> Option<isize> {
     let mut x: isize = 0;
     let mut y: isize = 0;
     for line in input.lines() {
         let s = line.split_once(" ").unwrap();
         let d = s.1.parse::<isize>().unwrap();
         match s.0 {
-            "forward" => {x+= d}
-            "up" => {y-= d}
-            "down" => {y+=d}
-            _=>{}
+            "forward" => x += d,
+            "up" => y -= d,
+            "down" => y += d,
+            _ => {}
         }
     }
     return Some(x * y);
 }
 
-pub fn part_two(input: &str) -> Option<impl Debug + Display + num::Integer> {
+pub fn part_two(input: &str) -> Option<isize> {
     let mut aim: isize = 0;
     let mut x: isize = 0;
     let mut y: isize = 0;
@@ -30,13 +29,16 @@ pub fn part_two(input: &str) -> Option<impl Debug + Display + num::Integer> {
         let s = line.split_once(" ").unwrap();
         let d = s.1.parse::<isize>().unwrap();
         match s.0 {
-            "forward" => {x+= d;y+=aim*d;}
-            "up" => {aim-= d}
-            "down" => {aim+=d}
-            _=>{}
+            "forward" => {
+                x += d;
+                y += aim * d;
+            }
+            "up" => aim -= d,
+            "down" => aim += d,
+            _ => {}
         }
     }
-    return Some(x * y); 
+    return Some(x * y);
 }
 
 #[cfg(test)]
@@ -46,12 +48,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(150));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(900));
     }
 }
